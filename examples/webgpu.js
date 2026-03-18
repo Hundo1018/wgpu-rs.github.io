a1e06299aa163dbcc7f47537ede329dd0e28056c
teoxoy <28601907+teoxoy@users.noreply.github.com>
2026-03-18 18:30:25 +0000

    Deploying to master from @ gfx-rs/wgpu@9a3bba69628cb3998c4e5a994c4bd11fe43c96cf 🚀

--- examples/webgpu.js
+++ examples/webgpu.js
@@ -1912,7 +1912,7 @@ function __wbg_get_imports() {
             arg0.writeTexture(arg1, getArrayU8FromWasm0(arg2, arg3), arg4, arg5);
         }, arguments); },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { dtor_idx: 1694, function: Function { arguments: [Externref], shim_idx: 3557, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { dtor_idx: 1694, function: Function { arguments: [Externref], shim_idx: 3560, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm.wasm_bindgen__closure__destroy__h1272303de8802af8, wasm_bindgen__convert__closures_____invoke__h812d665f4b115df5);
             return ret;
         },

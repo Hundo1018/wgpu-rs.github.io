5f8ec6cc678e12695e1c8d62b821c7bbf84c342c
teoxoy <28601907+teoxoy@users.noreply.github.com>
2026-03-23 20:23:08 +0000

    Deploying to master from @ gfx-rs/wgpu@d199493486ccf2090f73579036682b7e0d095355 🚀

--- examples/webgpu.js
+++ examples/webgpu.js
@@ -1971,12 +1971,12 @@ function __wbg_get_imports() {
             arg0.writeTexture(arg1, getArrayU8FromWasm0(arg2, arg3), arg4, arg5);
         }, arguments); },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { dtor_idx: 1516, function: Function { arguments: [Externref], shim_idx: 1186, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { dtor_idx: 1811, function: Function { arguments: [Externref], shim_idx: 1836, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm.wasm_bindgen__closure__destroy__hb1f43e23efed5506, wasm_bindgen__convert__closures_____invoke__hc5915a1af296abb9);
             return ret;
         },
         __wbindgen_cast_0000000000000002: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { dtor_idx: 1517, function: Function { arguments: [NamedExternref("GPUUncapturedErrorEvent")], shim_idx: 1187, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { dtor_idx: 1812, function: Function { arguments: [NamedExternref("GPUUncapturedErrorEvent")], shim_idx: 1837, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm.wasm_bindgen__closure__destroy__h5d06e6c6be47d33e, wasm_bindgen__convert__closures_____invoke__h885602c4644764bd);
             return ret;
         },

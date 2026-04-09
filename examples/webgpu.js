af7bc846f1901cd4c74d039f12fa1b694123564d
cwfitzgerald <connorwadefitzgerald@gmail.com>
2026-04-09 22:39:41 +0000

    Deploying to master from @ gfx-rs/wgpu@e6e5f5288cf4dc5b4a8a1447bce420f455ad3261 🚀

--- examples/webgpu.js
+++ examples/webgpu.js
@@ -1985,7 +1985,7 @@ function __wbg_get_imports() {
             return ret;
         },
         __wbindgen_cast_0000000000000003: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 3720, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 3722, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm_bindgen__convert__closures_____invoke__hc4ace964a3be2d49);
             return ret;
         },
